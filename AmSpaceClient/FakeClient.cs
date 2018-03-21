f8ee6fff9594b620a735e91ed233c7a369e46c0a
Artem <nexus.realman@gmail.com>
2018-03-21 11:16:50 +0100

    Fake client implementation; Issue #9

--- AmSpaceClient/FakeClient.cs
+++ AmSpaceClient/FakeClient.cs
@@ -12,26 +12,253 @@ namespace AmSpaceClient
     {
         public Task<IEnumerable<Competency>> GetCompetenciesAsync()
         {
-            throw new NotImplementedException();
+            IEnumerable<Competency> result = new List<Competency>
+            {
+                new Competency
+                {
+                    Id = 1,
+                    Name = "Decision Making",
+                    LevelId = 1
+                },
+                new Competency
+                {
+                    Id = 2,
+                    Name = "Decision Making",
+                    LevelId = 2
+                },
+                new Competency
+                {
+                    Id = 3,
+                    Name = "Communication",
+                    LevelId = 3
+                }
+            };
+            return Task.FromResult(result);
         }
 
         public Task<IEnumerable<CompetencyAction>> GetCompetencyActionsAsync(long competencyId)
         {
-            throw new NotImplementedException();
+            IEnumerable<CompetencyAction> result = new List<CompetencyAction>
+            {
+                new CompetencyAction
+                {
+                    Id = 1,
+                    Name = "Decision Making",
+                    LevelId = 1,
+                    Actions = new List<IdpAction>
+                    {
+                        new IdpAction
+                        {
+                            Id = 1,
+                            Name = "make deciscion dude",
+                            ActionType = new ActionType
+                            {
+                                Value = 10,
+                                DisplayName = "10%"
+                            },
+                            Translations = new List<Translation>
+                            {
+                                new Translation
+                                {
+                                    Id = 1,
+                                    Name = "make deciscion dude",
+                                    Language = "en-us"
+                                },
+                                new Translation
+                                {
+                                    Id = 2,
+                                    Name = "something in Polish",
+                                    Language = "pl-pl"
+                                }
+                            }
+                        },
+                        new IdpAction
+                        {
+                            Id = 2,
+                            Name = "make deciscion dude again",
+                            ActionType = new ActionType
+                            {
+                                Value = 20,
+                                DisplayName = "20%"
+                            },
+                            Translations = new List<Translation>
+                            {
+                                new Translation
+                                {
+                                    Id = 3,
+                                    Name = "make deciscion dude",
+                                    Language = "en-us"
+                                },
+                                new Translation
+                                {
+                                    Id = 4,
+                                    Name = "something in Polish",
+                                    Language = "pl-pl"
+                                }
+                            }
+                        }
+                    }
+                },
+                new CompetencyAction
+                {
+                    Id = 2,
+                    Name = "Decision Making",
+                    LevelId = 2,
+                    Actions = new List<IdpAction>
+                    {
+                        new IdpAction
+                        {
+                            Id = 3,
+                            Name = "make deciscion dude",
+                            ActionType = new ActionType
+                            {
+                                Value = 10,
+                                DisplayName = "10%"
+                            },
+                            Translations = new List<Translation>
+                            {
+                                new Translation
+                                {
+                                    Id = 5,
+                                    Name = "make deciscion dude",
+                                    Language = "en-us"
+                                },
+                                new Translation
+                                {
+                                    Id = 6,
+                                    Name = "something in Polish",
+                                    Language = "pl-pl"
+                                }
+                            }
+                        },
+                        new IdpAction
+                        {
+                            Id = 4,
+                            Name = "make deciscion dude again",
+                            ActionType = new ActionType
+                            {
+                                Value = 20,
+                                DisplayName = "20%"
+                            },
+                            Translations = new List<Translation>
+                            {
+                                new Translation
+                                {
+                                    Id = 7,
+                                    Name = "make deciscion dude",
+                                    Language = "en-us"
+                                },
+                                new Translation
+                                {
+                                    Id = 8,
+                                    Name = "something in Polish",
+                                    Language = "pl-pl"
+                                }
+                            }
+                        }
+                    }
+                },
+                new CompetencyAction
+                {
+                    Id = 3,
+                    Name = "Communication",
+                    LevelId = 3,
+                    Actions = new List<IdpAction>
+                    {
+                        new IdpAction
+                        {
+                            Id = 5,
+                            Name = "make deciscion dude",
+                            ActionType = new ActionType
+                            {
+                                Value = 10,
+                                DisplayName = "10%"
+                            },
+                            Translations = new List<Translation>
+                            {
+                                new Translation
+                                {
+                                    Id = 9,
+                                    Name = "make deciscion dude",
+                                    Language = "en-us"
+                                },
+                                new Translation
+                                {
+                                    Id = 10,
+                                    Name = "something in Polish",
+                                    Language = "pl-pl"
+                                }
+                            }
+                        },
+                        new IdpAction
+                        {
+                            Id = 6,
+                            Name = "make deciscion dude again",
+                            ActionType = new ActionType
+                            {
+                                Value = 20,
+                                DisplayName = "20%"
+                            },
+                            Translations = new List<Translation>
+                            {
+                                new Translation
+                                {
+                                    Id = 11,
+                                    Name = "make deciscion dude",
+                                    Language = "en-us"
+                                },
+                                new Translation
+                                {
+                                    Id = 12,
+                                    Name = "something in Polish",
+                                    Language = "pl-pl"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            return Task.FromResult(result);
         }
 
         public Task<IEnumerable<Level>> GetLevelsAsync()
         {
-            throw new NotImplementedException();
+            IEnumerable<Level> result = new List<Level>
+            {
+                new Level
+                {
+                    Id = 1,
+                    Name = "1"
+                },
+                new Level
+                {
+                    Id = 2,
+                    Name = "2"
+                },
+                new Level
+                {
+                    Id = 3,
+                    Name = "3"
+                }
+            };
+            return Task.FromResult(result);
         }
 
         public Task<bool> LoginRequestAsync(string userName, SecureString password) => Task.FromResult(true);
 
-        public Task<Profile> ProfileRequestAsync() => Task.FromResult(new Profile());
+        public Task<Profile> ProfileRequestAsync()
+        {
+            var result = new Profile
+            {
+                FirstName = "MyName",
+                LastName = "MySurname"
+            };
+            return Task.FromResult(result);
+        }
 
         public Task UpdateActionAsync(UpdateAction model, long competencyId)
         {
-            throw new NotImplementedException();
+            return Task.CompletedTask;
         }
     }
 }
