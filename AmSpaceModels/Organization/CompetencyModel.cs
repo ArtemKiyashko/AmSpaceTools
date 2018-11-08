bcac5762306286c11f3b77a8158a33b55b2a7f50
ArtemKiyashko <artem.kiyashko@amrest.eu>
2018-11-08 13:12:34 +0100

    competency model upload. tool part

---
+++ AmSpaceModels/Organization/CompetencyModel.cs
@@ -0,0 +1,51 @@
+﻿using AmSpaceModels.Idp;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmSpaceModels.Organization
+{
+    public partial class Competencies
+    {
+        [JsonProperty("competencies")]
+        public ICollection<CompetencyModel> CompetenciesList { get; set; }
+    }
+    public partial class CompetencyModel
+    {
+        [JsonProperty("domain")]
+        public long Domain { get; set; }
+
+        [JsonProperty("level")]
+        public long Level { get; set; }
+
+        [JsonProperty("translations")]
+        public ICollection<CompetencyModelTranslation> Translations { get; set; }
+
+        [JsonProperty("behaviors")]
+        public ICollection<Behavior> Behaviors { get; set; }
+
+        [JsonProperty("pk")]
+        public long? Id { get; set; }
+    }
+
+    public partial class CompetencyModelTranslation
+    {
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public partial class Behavior
+    {
+        [JsonProperty("translations")]
+        public ICollection<CompetencyModelTranslation> Translations { get; set; }
+
+        [JsonProperty("pk")]
+        public long? Id { get; set; }
+    }
+}
