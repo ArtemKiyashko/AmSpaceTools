0a9f15e917dab85ab54bbbc02e78b5fa71237add
ArtemKiyashko <artem.kiyashko@amrest.eu>
2018-11-08 13:13:20 +0100

    competency model excel part. initial implementation

---
+++ ExcelWorker/Models/CompetencyModelExcelRow.cs
@@ -0,0 +1,27 @@
+﻿using EPPlus.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWorker.Models
+{
+    public class CompetencyModelExcelRow
+    {
+        [ExcelTableColumn]
+        public int Level { get; set; }
+
+        [ExcelTableColumn]
+        public string Brand { get; set; }
+
+        [ExcelTableColumn]
+        public string Competency { get; set; }
+
+        [ExcelTableColumn]
+        public string Behavior { get; set; }
+
+        [ExcelTableColumn]
+        public string Language { get; set; }
+    }
+}
---
+++ ExcelWorker/Models/CoreValueExcelRow.cs
@@ -0,0 +1,22 @@
+﻿using EPPlus.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWorker.Models
+{
+    public class CoreValueExcelRow
+    {
+        [ExcelTableColumn]
+        public string Level { get; set; }
+        [ExcelTableColumn]
+        public string Title { get; set; }
+        [ExcelTableColumn]
+        public string Description { get; set; }
+
+        [ExcelTableColumn("Lang")]
+        public string Language { get; set; }
+    }
+}
