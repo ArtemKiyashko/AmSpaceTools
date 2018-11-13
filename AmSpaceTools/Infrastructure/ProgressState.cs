6b9fb5a37fea439cda2a512e83b2dda983090eb9
Aleksey Knyazev <joalexey@gmail.com>
2018-11-13 14:40:28 +0300

    get rid of state property

--- AmSpaceTools/Infrastructure/ProgressState.cs
+++ AmSpaceTools/Infrastructure/ProgressState.cs
@@ -8,22 +8,14 @@ namespace AmSpaceTools.Infrastructure
 {
     public interface IProgressState
     {
-        Status ProgressStatus { get; set; }
         int ProgressPercent { get ; }
         int ProgressTasksDone { get; set; }
         int ProgressTasksTotal { get; set; }
 
     }
 
-    public enum Status
-    {
-        Preparations,
-        Uploading,
-        Finishing
-    }
     public struct ProgressState : IProgressState
     {
-        public Status ProgressStatus { get; set; }
         public int ProgressPercent => Convert.ToInt32(ProgressTasksTotal != 0 ? (double)ProgressTasksDone / (double)ProgressTasksTotal * 100 : 100);
         public int ProgressTasksDone { get ; set ; }
         public int ProgressTasksTotal { get ; set ; }
