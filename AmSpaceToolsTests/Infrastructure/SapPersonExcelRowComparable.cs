2c522bd1bee2246544dead4d05cf862e16cfa162
Artem Kiyashko <artem.kiyashko@amrest.eu>
2018-11-29 12:53:33 +0100

    make ternary operator in return statement

--- AmSpaceToolsTests/Infrastructure/SapPersonExcelRowComparable.cs
+++ AmSpaceToolsTests/Infrastructure/SapPersonExcelRowComparable.cs
@@ -15,23 +15,21 @@ namespace AmSpaceToolsTests.Infrastructure
             var res = obj as ExternalAccount;
             if (res == null) return -1;
 
-            if (!res.FirstName.Equals(Name)
-                || !res.LastName.Equals(Surname)
-                || (int)res.Status != (int)Status
-                || (int)res.Sex != (int)Sex
-                || !res.PhoneNumber.Equals(Phone)
-                || !res.Nationality.Equals(Nationality)
-                || !res.CountryCode.Equals(Country)
-                || !res.Mpk.Equals(Mpk)
-                || !res.Level.Equals(Level)
-                || !res.PersonLegalId.Equals(IdentityNumber)
-                || !res.StartDate.Equals(ContractStartDate)
-                || !res.EndDate.Equals(ContractEndDate)
-                || !res.ContractNumber.Equals(ContractNumber)
-                || !res.DateOfBirth.Equals(BirthDate)
-                || !res.Email.Equals(Email))
-                return -1;
-            return 0;
+            return (!res.FirstName.Equals(Name)
+               || !res.LastName.Equals(Surname)
+               || (int)res.Status != (int)Status
+               || (int)res.Sex != (int)Sex
+               || !res.PhoneNumber.Equals(Phone)
+               || !res.Nationality.Equals(Nationality)
+               || !res.CountryCode.Equals(Country)
+               || !res.Mpk.Equals(Mpk)
+               || !res.Level.Equals(Level)
+               || !res.PersonLegalId.Equals(IdentityNumber)
+               || !res.StartDate.Equals(ContractStartDate)
+               || !res.EndDate.Equals(ContractEndDate)
+               || !res.ContractNumber.Equals(ContractNumber)
+               || !res.DateOfBirth.Equals(BirthDate)
+               || !res.Email.Equals(Email)) ? -1 : 0;
         }
     }
 }
