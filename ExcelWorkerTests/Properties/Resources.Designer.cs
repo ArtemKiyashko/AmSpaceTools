638bd9f7c287656c17973bb311df62b5bbe7f2f9
Artem Kiyashko <artem.kiyashko@amrest.eu>
2018-12-27 15:54:13 +0100

    ExcelWorker remove old tests and files

--- ExcelWorkerTests/Properties/Resources.Designer.cs
+++ ExcelWorkerTests/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ExcelWorkerTests.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] example {
-            get {
-                object obj = ResourceManager.GetObject("example", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
